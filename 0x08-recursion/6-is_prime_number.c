67ae749ab8e275de5530dd82eaaa2367a355e5b5
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-09-29 14:22:06 +0300

    first ever try

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-     int is_prime(signed int n, signed in c )
-     {
-        if (n%c == 0)
-        {
-            if (n==c )
-                return (1);
-            else 
-               return (0);
-        }
-        return (0+is_prime_number(n, c+1));
-
- }
- /**
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
  * is_prime_number - detects if an input number is a prime number.
  * @n: input number.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
- int is_prime_number(int n)
+int is_prime_number(int n)
 {
 	if (n == 0)
 		return (0);
@@ -32,4 +32,3 @@
 		return (0);
 	return (is_prime(n, 2));
 }
-
