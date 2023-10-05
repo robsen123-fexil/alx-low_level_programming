33853d72f4848f36b978b42b84570648a4e20731
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-10-05 10:34:23 +0300

    robsne

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (x = 0; x < height; x++)
+		free(grid[x]);
+
+	free(grid);
+}
