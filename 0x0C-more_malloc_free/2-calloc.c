ccdce702cfd1e750d5d33aa2dd70855641c9e2ab
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-10-06 22:02:01 +0300

    changed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,12 +2,11 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return: pointer to the memory area s
+ * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
